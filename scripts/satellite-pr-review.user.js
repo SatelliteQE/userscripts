0b8d9ed797a8f25be3e3ceb364a115654413fc49
Mirosław Zalewski <mzalewsk@redhat.com>
2019-06-03 12:57:56 +0200

    pr-review: fix reviewers list on new pr page

--- scripts/satellite-pr-review.user.js
+++ scripts/satellite-pr-review.user.js
@@ -3,7 +3,7 @@
 // @namespace   SatelliteQE
 // @description Helps PR review process in SatelliteQE projects
 // @match       https://github.com/SatelliteQE/*
-// @version     1.5
+// @version     1.6
 // @run-at      document-end
 // ==/UserScript==
 
@@ -154,6 +154,7 @@ const addCustomStyles = function() {
 
     const styleRules = [
         '#satelliteqe-reviewers {position: absolute; right: 300px;}',
+        '.discussion-sidebar > #satelliteqe-reviewers {position: absolute; right: -200px;}',
         '#satelliteqe-reviewers h4 {margin-top: 1em;}',
         '#satelliteqe-process-checks li {margin-left: 1.2em;}'
     ];
